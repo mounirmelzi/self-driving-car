74c6ed1a6071b3ef9a16cb0f452fabe62e6c1657
Mounir <lm_melzi@esi.dz>
2023-11-11 15:54:38 +0100

    fix car sensor draw

--- main.js
+++ main.js
@@ -71,15 +71,15 @@ function animate(time) {
 
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(carCtx, "red");
+    traffic[i].draw(carCtx);
   }
 
   carCtx.globalAlpha = 0.2;
   for (let i = 0; i < cars.length; i++) {
-    cars[i].draw(carCtx, "blue");
+    cars[i].draw(carCtx);
   }
   carCtx.globalAlpha = 1;
-  bestCar.draw(carCtx, "blue", true);
+  bestCar.draw(carCtx, true);
 
   carCtx.restore();
 
